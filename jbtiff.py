184fc4183464af81c429ce6c9d21aa69a0fa6d01
Johann A. Briffa <johann.briffa@um.edu.mt>
2015-08-25 12:12:31 +0200

    Implemented writing method

--- jbtiff.py
+++ jbtiff.py
@@ -84,6 +84,53 @@ class tiff_file():
       # convert and return
       return struct.unpack(fmt, tmp)[0]
 
+   # write a word of given size (in bytes), endianness, and signed/unsigned state
+   @staticmethod
+   def write_word(data, fid, size, signed, little_endian):
+      # set up endianness
+      if little_endian:
+         fmt = '<'
+      else:
+         fmt = '>'
+      # set up word size
+      if size == 1:
+         fmt += 'B'
+      elif size == 2:
+         fmt += 'H'
+      elif size == 4:
+         fmt += 'L'
+      else:
+         raise ValueError('Unsupported word size: %d' % size)
+      # set up signed/unsigned nature
+      if signed:
+         fmt = fmt.lower()
+      # convert and write
+      buf = struct.pack(fmt, data)
+      assert len(buf) == size
+      fid.write(buf)
+      return
+
+   # write a real number of given size (in bytes) and endianness
+   @staticmethod
+   def write_float(data, fid, size, little_endian):
+      # set up endianness
+      if little_endian:
+         fmt = '<'
+      else:
+         fmt = '>'
+      # set up word size
+      if size == 4:
+         fmt += 'f'
+      elif size == 8:
+         fmt += 'd'
+      else:
+         raise ValueError('Unsupported float size: %d' % size)
+      # convert and write
+      buf = struct.pack(fmt, data)
+      assert len(buf) == size
+      fid.write(buf)
+      return
+
    ## class methods
 
    # initialize class from stream
@@ -108,10 +155,11 @@ class tiff_file():
          # check if this was the last one
          if ifd_offset == 0:
             break
-         # read IFD entries
-         IFD = []
+         # read number of IFD entries
          fid.seek(ifd_offset)
          entry_count = self.read_word(fid, 2, False, self.little_endian)
+         # read IFD entries
+         IFD = []
          for i in range(entry_count):
             # make sure we're in the correct position
             fid.seek(ifd_offset + 2 + i*12)
@@ -156,3 +204,93 @@ class tiff_file():
          self.IFDs.append(IFD)
          # make sure we're in the correct position to read next offset
          fid.seek(ifd_offset + 2 + entry_count*12)
+      return
+
+   # write data to stream
+   def write(self, fid):
+      # byte order
+      if self.little_endian:
+         fid.write('II')
+      else:
+         fid.write('MM')
+      # TIFF identifier
+      self.write_word(42, fid, 2, False, self.little_endian)
+      # initialize pointers to offset and to next free space
+      offset_ptr = 4
+      free_ptr = 8
+      # write all IFDs in file
+      for IFD in self.IFDs:
+         # write offset to this IFD
+         ifd_offset = free_ptr
+         fid.seek(offset_ptr)
+         self.write_word(ifd_offset, fid, 4, False, self.little_endian)
+         # write number of IFD entries
+         fid.seek(ifd_offset)
+         entry_count = len(IFD)
+         self.write_word(entry_count, fid, 2, False, self.little_endian)
+         # update pointer to offset and to next free space
+         offset_ptr = ifd_offset + 2 + entry_count*12
+         free_ptr =  offset_ptr + 4
+         # write IFD entries
+         for i, (tag, field_type, values) in enumerate(IFD):
+            # make sure we're in the correct position
+            fid.seek(ifd_offset + 2 + i*12)
+            # write IFD entry information
+            self.write_word(tag, fid, 2, False, self.little_endian)
+            self.write_word(field_type, fid, 2, False, self.little_endian)
+            # determine count
+            if field_type == 2: # ASCII
+               value_count = sum([len(x)+1 for x in values])
+            else:
+               value_count = len(values)
+            self.write_word(value_count, fid, 4, False, self.little_endian)
+            # check if the value fits here or if we need offset
+            if self.field_size[field_type] * value_count > 4:
+               value_offset = free_ptr
+               self.write_word(value_offset, fid, 4, False, self.little_endian)
+               fid.seek(value_offset)
+               free_ptr += self.field_size[field_type] * value_count
+            # write value(s)
+            if field_type == 1: # BYTE
+               for value in values:
+                  self.write_word(value, fid, 1, False, self.little_endian)
+            elif field_type == 2: # ASCII
+               for value in values:
+                  fid.write(value)
+                  self.write_word(0, fid, 1, False, self.little_endian)
+            elif field_type == 3: # SHORT
+               for value in values:
+                  self.write_word(value, fid, 2, False, self.little_endian)
+            elif field_type == 4: # LONG
+               for value in values:
+                  self.write_word(value, fid, 4, False, self.little_endian)
+            elif field_type == 5: # RATIONAL
+               for value in values:
+                  self.write_word(value[0], fid, 4, False, self.little_endian)
+                  self.write_word(value[1], fid, 4, False, self.little_endian)
+            elif field_type == 6: # SBYTE
+               for value in values:
+                  self.write_word(value, fid, 1, True, self.little_endian)
+            elif field_type == 7: # UNDEFINED
+               for value in values:
+                  self.write_word(value, fid, 1, False, self.little_endian)
+            elif field_type == 8: # SSHORT
+               for value in values:
+                  self.write_word(value, fid, 2, True, self.little_endian)
+            elif field_type == 9: # SLONG
+               for value in values:
+                  self.write_word(value, fid, 4, True, self.little_endian)
+            elif field_type == 10: # SRATIONAL
+               for value in values:
+                  self.write_word(value[0], fid, 4, True, self.little_endian)
+                  self.write_word(value[1], fid, 4, True, self.little_endian)
+            elif field_type == 11: # FLOAT
+               for value in values:
+                  self.write_float(value, fid, 4, self.little_endian)
+            elif field_type == 12: # DOUBLE
+               for value in values:
+                  self.write_float(value, fid, 8, self.little_endian)
+      # write null offset to IFD
+      fid.seek(offset_ptr)
+      self.write_word(0, fid, 4, False, self.little_endian)
+      return
