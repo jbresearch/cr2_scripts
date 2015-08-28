0c894a2ecb1c2b7f69cedaf778821cd348a5f73c
Johann A. Briffa <johann.briffa@um.edu.mt>
2015-08-28 09:32:49 +0200

    Reordering and formatting

--- process_raw.py
+++ process_raw.py
@@ -42,14 +42,21 @@ def main():
                      help="display interpreted image")
    args = parser.parse_args()
 
-   # determine the number of slices and the width of each slice
-   slices = (args.width + args.slice - 1) // args.slice
-   slice_widths = [args.slice] * (slices-1) + [args.width - args.slice*(slices-1)]
+   # Laurent Clévy's example:
+   # Image (w x h): 5184 x 3456
+   # 4 color components (w x h): 0x538 x 0xdbc = 1336 x 3516 each
+   #    interleaved components: 5344 x 3516
+   #    border: 160 x 60 on declared image size
+   # 3 slices (w): 2x 0x6c0 + 0x760 = 2x 1728 + 1888 = 5344
+   #    each slice takes: 432 pixels from each of 4 colors (first two)
+   #                      472 pixels from each of 4 colors (last one)
+
    # convert lossless JPEG encoded input file to raw data
    cmd = 'pvrg-jpeg -d -s "%s" -o parts' % args.input
    st, out = commands.getstatusoutput(cmd)
    if st != 0:
       raise AssertionError('Error decoding JPEG file: %s' % out)
+
    # interpret output to determine color components
    components = []
    for line in out.split('\n'):
@@ -62,14 +69,6 @@ def main():
          components.append((f,w,h))
    # number of color components
    n = len(components)
-   # Laurent Clévy's example:
-   # Image (w x h): 5184 x 3456
-   # 4 color components (w x h): 0x538 x 0xdbc = 1336 x 3516 each
-   #    interleaved components: 5344 x 3516
-   #    border: 160 x 60 on declared image size
-   # 3 slices (w): 2x 0x6c0 + 0x760 = 2x 1728 + 1888 = 5344
-   #    each slice takes: 432 pixels from each of 4 colors (first two)
-   #                      472 pixels from each of 4 colors (last one)
    # first assemble color components
    assert all([h == args.height for f,w,h in components])
    assert sum([w for f,w,h in components]) == args.width
@@ -80,12 +79,17 @@ def main():
       b.shape = (h,w)
       # insert into assembled color image
       a[:,i::n] = b
+
+   # determine the number of slices and the width of each slice
+   slices = (args.width + args.slice - 1) // args.slice
+   slice_widths = [args.slice] * (slices-1) + [args.width - args.slice*(slices-1)]
    # next unslice image
    I = np.zeros((args.height, args.width), dtype=np.dtype('>H'))
    for i, sw in enumerate(slice_widths):
       col_s = sum(slice_widths[0:i])
       col_e = col_s + sw
       I[:,col_s:col_e] = a.flat[col_s*args.height:col_e*args.height].reshape(args.height,sw)
+
    # show user what we've done, as needed
    if args.display:
       # linear display
