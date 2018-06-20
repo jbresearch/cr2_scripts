881e436d414ad01215ca7aafc21951d499ed9cc3
Johann A. Briffa <johann.briffa@um.edu.mt>
2018-06-20 08:33:09 +0200

    Script to download sample image

---
+++ example/download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright © 2015-2017 Johann A. Briffa
+#
+# This file is part of CR2_Scripts.
+#
+# CR2_Scripts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CR2_Scripts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CR2_Scripts.  If not, see <http://www.gnu.org/licenses/>.
+
+wget -O 2908.cr2 "https://drive.google.com/uc?export=download&id=0B1XtGKkZqU5SZ2l1YmtwYzRiLU0"
